998b1406e6d8b4d31d77fdcec9122bc158414498
GMatrixGames <gmatrixGames@hotmail.com>
2019-06-18 20:15:44 -0400

    Update some Paper stuff

---
+++ src/main/java/org/spigotmc/AsyncCatcher.java
@@ -0,0 +1,22 @@
+package org.spigotmc;
+
+import net.minecraft.server.MinecraftServer;
+
+public class AsyncCatcher
+{
+
+    public static boolean enabled = true;
+
+    public static void catchOp(String reason)
+    {
+        if ( enabled && Thread.currentThread() != MinecraftServer.getServerInst().primaryThread )
+        {
+            throw new IllegalStateException( "Asynchronous " + reason + "!" );
+        }
+    }
+
+    public static boolean catchInv()
+    {
+        return enabled && Thread.currentThread() != MinecraftServer.getServerInst().primaryThread;
+    }
+}
