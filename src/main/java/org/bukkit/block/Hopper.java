7cf2fc25c81b9d638fb522b54776e720589c1e21
GMatrixGames <gmatrixgames@gmail.com>
2019-06-19 19:56:59 -0300

    Add empty directory, for now

--- src/main/java/org/bukkit/block/Hopper.java
+++
@@ -1,9 +0,0 @@
-package org.bukkit.block;
-
-import org.bukkit.Nameable;
-import com.destroystokyo.paper.loottable.LootableInventory; // Paper
-
-/**
- * Represents a captured state of a hopper.
- */
-public interface Hopper extends Container, Nameable, LootableInventory { } // Paper
