9fab74da336b244e04d459d4d7497de34420f5c9
Garrett <gmatrixgames@hotmail.com>
2019-06-19 20:54:36 -0300

    Update JavaPluginLoader.java

--- src/main/java/org/bukkit/plugin/java/JavaPluginLoader.java
+++ src/main/java/org/bukkit/plugin/java/JavaPluginLoader.java
@@ -289,7 +289,6 @@ public final class JavaPluginLoader implements PluginLoader {
                 }
             }
 
-            EventExecutor executor = new co.aikar.timings.TimedEventExecutor(EventExecutor.create(method, eventClass), plugin, method, eventClass); // Spigot // Paper - Use factory method `EventExecutor.create()`
             if (false) { // Spigot - RL handles useTimings check now
                 eventSet.add(new TimedRegisteredListener(listener, executor, eh.priority(), plugin, eh.ignoreCancelled()));
             } else {
