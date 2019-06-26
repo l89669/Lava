d162607ccdbaf0bb2d700aaf03f19b301c4f1857
Garrett <gmatrixgames@hotmail.com>
2019-06-26 20:29:54 -0300

    Update VersionCommand.java

--- src/main/java/org/bukkit/command/defaults/VersionCommand.java
+++ src/main/java/org/bukkit/command/defaults/VersionCommand.java
@@ -231,10 +231,7 @@ public class VersionCommand extends BukkitCommand {
         } catch (NumberFormatException ex) {
             verInfo = verInfo.replace("\"", "");
             return getFromRepo("LavaPowered/Lava", verInfo);
-        } 
-
-        verInfo = verInfo.replace("\"", "");
-        return getFromRepo("LavaPowered/Lava", verInfo);
+        }
     }
 
     private static int getFromJenkins(int currentVer) {
