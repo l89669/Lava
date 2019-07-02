b6c9b6bb1853248086418f2483a9b40582f248bb
GMatrixGames <gmatrixgames@hotmail.com>
2019-07-02 17:06:27 -0300

    Fix remapper issue

--- src/main/java/com/maxqia/remapper/Utils.java
+++ src/main/java/com/maxqia/remapper/Utils.java
@@ -25,32 +25,22 @@ public class Utils {
         return name;
     }
 
-    //TODO: check return type?
     public static String mapMethodInternal(Class<?> inst, String name, Class<?>... parameterTypes) {
         String match = reverseMap(inst) + "/" + name;
 
         Map<String, String> map = Transformer.jarMapping.methods;
         for (String value : map.keySet()) {
-            if (value.startsWith(match)) {
-                String params = value.split("\\s+")[1];
-                Type[] types = Type.getArgumentTypes(params);
-                
-                if (types.length != parameterTypes.length) { //skip entry if number of params doesn't match - a bit more effective
-                	continue;
-                }
-              
-                int i;
-
-                for (i = 0; i < types.length; i++) {
-                    if (!types[i].getClassName().replace('$', '.').replace('/', '.').equals(reverseMapExternal(parameterTypes[i]))) { //add replace to avoid problems with subclasses and $ char
-                        i = -1;
-                        break;
-                    }
+            String[] str = value.split("\\s+");
+            int i = 0;
+            for (Type type : Type.getArgumentTypes(str[1])) {
+                if (i >= parameterTypes.length || !type.getClassName().equals(reverseMapExternal(parameterTypes[i]))) {
+                    i = -1;
+                    break;
                 }
                 i++;
             }
 
-            if (i > -1) {
+            if (i >= parameterTypes.length) {
                 return map.get(value);
             }
         }
