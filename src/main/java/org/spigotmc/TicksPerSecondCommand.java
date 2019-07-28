07d947a108d9f61eff8475cfd3d44a0827c83fc1
Garrett <gmatrixgames@hotmail.com>
2019-07-28 18:43:23 -0300

    Fix tps cmd super call

--- src/main/java/org/spigotmc/TicksPerSecondCommand.java
+++ src/main/java/org/spigotmc/TicksPerSecondCommand.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 public class TicksPerSecondCommand extends Command {
 
     public TicksPerSecondCommand(String name) {
-        super("name");
+        super(name);
         this.description = "Gets the current ticks per second for the server";
         this.usageMessage = "/tps";
         setPermission("bukkit.command.tps");
